fb5262410669034f768011e61184dd2bc9af6283
Vasiliy Denisov <v.denisov@id-network.ru>
2020-03-11 11:19:53 +0300

    create_interested_parties.py

--- rais/create_interested_parties.py
+++ rais/create_interested_parties.py
@@ -40,7 +40,7 @@ class CreateInterestedParties:
     @classmethod
     def person(self, type_person, prefix='_'):
         ip_person = ip.person()[type_person]
-        name_ip = pi.get_prefix()+ip_person["name_first"]
+        name_ip = pi.get_prefix()+prefix+ip_person["name_first"]
         c_a = self.check_availability(name_ip=name_ip)
         if c_a :
             response = self.get_contragent_id(c_a)
@@ -60,7 +60,7 @@ class CreateInterestedParties:
                 "object_info": ip_person["object_info"]
             }
             cr.post(url=pi.get_url_host() + '/api/red/contragent/add', params=params)
-            print('Создали заинтересованную сторону ' + pi.get_prefix()+ip_person["name_first"])
+            print('Создали заинтересованную сторону ' + pi.get_prefix()+prefix+ip_person["name_first"])
             c_a = self.check_availability(name_ip=name_ip)
             response = self.get_contragent_id(c_a)
         return response.json()["data"]["item"]
